4ac15bc14dba03a34812dbfff169d40a0acda51c
Totostore <totostore@gmail.com>
2017-10-27 12:26:24 +0200

    MAP V1.1

--- fichiers/page/map.php
+++ fichiers/page/map.php
@@ -6,6 +6,34 @@
   </head>
 
   <body>
+
+  	<div class="row">
+  		<div class="col-sm-12  col-lg-12 nav">
+          <div class="navbar-wrapper">
+            <nav class="navbar navbar-inverse navbar-static-top">
+              <div class="container">
+                <div class="navbar-header">
+                  <a class="navbar-brand" href="accueil.php"> Home </a>
+                </div>
+                <div id="navbar" class="navbar-collapse collapse">
+                <ul class="nav navbar-nav">
+                  <li>
+                    <a href="map.php">MAP</a>
+                  </li>
+                  <li>
+                    <a href="comparatif.php">Requetes</a>
+                  </li>
+                  <li>
+                    <a href="contact.php">contact</a>
+                  </li>
+                </ul>
+                </div>
+              </div>
+            </nav>
+          </div>
+      </div>
+    </div>
+
     <h1>Je préfèrerais etre la-bas plutot qu'ici...</h1>
 	<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2607.9736431813303!2d-0.3676239179320031!3d49.18208674677159!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x480a42955f5b46eb%3A0x33dddc558bb641e5!2sLe+VERTIGO!5e0!3m2!1sfr!2sfr!4v1509098441749" width="800" height="600" frameborder="0" style="border:0" allowfullscreen></iframe>
 
