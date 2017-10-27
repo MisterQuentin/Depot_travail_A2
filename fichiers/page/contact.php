d17ee6f6ae302b353f787bca5e3fde94a0b28ecf
Totostore <totostore@gmail.com>
2017-10-27 14:04:27 +0200

    Contact

---
+++ fichiers/page/contact.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>La BIBINE C'EST ICI</title>
+  </head>
+
+  <body>
+
+    <h1> Nous trouver: </h1>
+	<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2607.9736431813303!2d-0.3676239179320031!3d49.18208674677159!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x480a42955f5b46eb%3A0x33dddc558bb641e5!2sLe+VERTIGO!5e0!3m2!1sfr!2sfr!4v1509098441749" width="800" height="600" frameborder="0" style="border:0" allowfullscreen></iframe>
+
+	<p> Nous contacter : </p>
+
+</body>
+</html>
+
