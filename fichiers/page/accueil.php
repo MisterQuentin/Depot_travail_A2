f7d08d779df7fc221fc562b6adc205052d7dbd4d
Totostore <totostore@gmail.com>
2017-10-27 15:13:54 +0200

    Contact

--- fichiers/page/accueil.php
+++ fichiers/page/accueil.php
@@ -2,10 +2,33 @@
 <html>
   <head>
     <meta charset="utf-8">
+    <link rel="stylesheet" href="../css/accueil.css">
     <title>LA BIBINE C'EST ICI</title>
   </head>
   <body>
-    <h1>FEMMME!!! A boire!</h1>
-    <p>je fais un petit test pour verifier et voir ce que je peux faire</p>
+    <h1>Le site des alcooliques</h1>
+    <div id="navbar">
+        <a href="accueil.php"><div id="titre" class="menu">ACCUEIL</div></a>
+        <a href="map.php"><div id="map" class="menu">MAP</div></a>
+        <a href="recherche_biere.php"><div id="requette" class="menu">RECHERCHE</div></a>
+        <a href="contact.php"><div id="contact" class="menu">CONTACT</div></a>
+        
+    </div>
+
+    <div id="content">
+    <div id="picture"></div>
+    <div class="text">
+    <p>Quid enim tam absurdum quam delectari multis inanimis rebus, ut honore, ut gloria, ut aedificio, ut vestitu cultuque corporis, animante virtute praedito, eo qui vel amare vel, ut ita dicam, redamare possit, non admodum delectari? Nihil est enim remuneratione benevolentiae, nihil vicissitudine studiorum officiorumque iucundius.</p>
+
+Cognitis enim pilatorum caesorumque funeribus nemo deinde ad has stationes appulit navem, sed ut Scironis praerupta letalia declinantes litoribus Cypriis contigui navigabant, quae Isauriae scopulis sunt controversa.
+
+Eodem tempore Serenianus ex duce, cuius ignavia populatam in Phoenice Celsen ante rettulimus, pulsatae maiestatis imperii reus iure postulatus ac lege, incertum qua potuit suffragatione absolvi, aperte convictus familiarem suum cum pileo, quo caput operiebat, incantato vetitis artibus ad templum misisse fatidicum, quaeritatum expresse an ei firmum portenderetur imperium, ut cupiebat, et cunctum.
+
+<p>Utque aegrum corpus quassari etiam levibus solet offensis, ita animus eius angustus et tener, quicquid increpuisset, ad salutis suae dispendium existimans factum aut cogitatum, insontium caedibus fecit victoriam luctuosam.
+
+Cum autem commodis intervallata temporibus convivia longa et noxia coeperint apparari vel distributio sollemnium sportularum, anxia deliberatione tractatur an exceptis his quibus vicissitudo debetur, peregrinum invitari conveniet, et si digesto plene consilio id placuerit fieri, is adhibetur qui pro domibus excubat aurigarum aut artem tesserariam profitetur aut secretiora quaedam se nosse confingit.</p>
+    </div>
+    </div>
+    <div id="footer">IMIE 2017 - ©ALL RIGHT RESERVED - Tout pompage de ce site sera punis!</div>
   </body>
 </html>
--- fichiers/page/contact.php
+++ fichiers/page/contact.php
@@ -2,15 +2,34 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>La BIBINE C'EST ICI</title>
+    <link rel="stylesheet" href="../css/contact.css">
+    <title>LA BIBINE C'EST ICI</title>
   </head>
-
   <body>
+    <h1>Le site des alcooliques</h1>
+    <div id="navbar">
+        <a href="accueil.php"><div id="titre" class="menu">ACCUEIL</div></a>
+        <a href="map.php"><div id="map" class="menu">MAP</div></a>
+        <a href="recherche_biere.php"><div id="requette" class="menu">RECHERCHE</div></a>
+        <a href="contact.php"><div id="contact" class="menu">CONTACT</div></a>
+        
+    </div>
 
-    <h1> Nous trouver: </h1>
-	<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2607.9736431813303!2d-0.3676239179320031!3d49.18208674677159!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x480a42955f5b46eb%3A0x33dddc558bb641e5!2sLe+VERTIGO!5e0!3m2!1sfr!2sfr!4v1509098441749" width="800" height="600" frameborder="0" style="border:0" allowfullscreen></iframe>
+   	<div id="contact">
+   		<div id="info">
+   			<div class="text">
+   				<h1> Nous contacter : </h1>
+   				<p> mail: Contact@Binouze-Caen.com /n<p>
+   				<p> tel: 02 31 69 69 69</p>
+   				<a href="https://i.skyrock.net/6436/40486436/pics/3249466060_1_3_ZvcILmRM.jpg"><img src="../Ressources/FB.jpeg" class="logo"></a>
+   				<a href="http://www.federation-peche14.fr/"><img src="../Ressources/truit.jpg" class="logo"></a>
+   			</div>
+   		</div>
+   		<div id="carte">
+			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2607.9736431813303!2d-0.3676239179320031!3d49.18208674677159!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x480a42955f5b46eb%3A0x33dddc558bb641e5!2sLe+VERTIGO!5e0!3m2!1sfr!2sfr!4v1509098441749" width="800" height="600" frameborder="0" style="border:0" allowfullscreen></iframe>
+   		</div>
+   	</div>
 
-	<p> Nous contacter : </p>
 
 </body>
 </html>
