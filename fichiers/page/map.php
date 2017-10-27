ad3a0ffa982fa235dd8546a8d8b859e2d465f6f2
Totostore <totostore@gmail.com>
2017-10-27 11:31:03 +0200

    pas grand chose

---
+++ fichiers/page/map.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Carte des Bars</title>
+  </head>
+  <body>
+    <h1>Mort De Reich !</h1>
+    <p>prout caca</p>
+  </body>
+</html>
