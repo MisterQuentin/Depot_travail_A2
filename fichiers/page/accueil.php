2d5ada026b4810fc4ec093c4002fb282e595267e
Unknown <corentinfaisien@hotmail.fr>
2017-10-27 11:22:19 +0200

    test
    page accueil

--- fichiers/page/accueil.php
+++ fichiers/page/accueil.php
@@ -2,10 +2,11 @@
 <html>
   <head>
     <meta charset="utf-8">
+    <link rel="stylesheet" href="../css/accueil.css">
     <title>LA BIBINE C'EST ICI</title>
   </head>
   <body>
-    <h1>FEMMME!!! A boire!</h1>
+    <h1>Le site des alcooliques</h1>
     <p>je fais un petit test pour verifier et voir ce que je peux faire</p>
   </body>
 </html>
