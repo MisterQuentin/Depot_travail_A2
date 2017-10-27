cbc88424751aac819d431ab667f9d08b2a0735d9
Unknown <corentinfaisien@hotmail.fr>
2017-10-27 15:02:09 +0200

    Page map

--- fichiers/page/map.php
+++ fichiers/page/map.php
@@ -10,6 +10,8 @@
         <a href="recherche_biere.php"><div id="requette" class="menu">RECHERCHE</div></a>
         <a href="contact.php"><div id="contact" class="menu">CONTACT</div></a>
     </div>
+    <div id="content">
     <div id="ggmap">
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d35086.68355397028!2d-0.3936269591148652!3d49.184748682236474!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x480a42bd4c04c933%3A0x3da5749f30d00859!2s14000+Caen!5e0!3m2!1sfr!2sfr!4v1509108528352" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe></div>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d35086.68355397028!2d-0.3936269591148652!3d49.184748682236474!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x480a42bd4c04c933%3A0x3da5749f30d00859!2s14000+Caen!5e0!3m2!1sfr!2sfr!4v1509108528352" width="500" height="450" frameborder="0" style="border:0" allowfullscreen></iframe></div>
+    <img src="../Ressources/Capture.PNG" style="width:500px;margin-top:20px;margin-left:20px;height:400px"></div>
 </html>
