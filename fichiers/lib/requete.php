75b005d5c623d8fe348910524519da3aa3113537
Maxime Leblond <maxime.leblond76@gmail.com>
2017-10-27 14:59:44 +0200

    passage objet pour la requete et ajout dans gitignore

--- fichiers/lib/requete.php
+++ fichiers/lib/requete.php
@@ -1,23 +1,27 @@
 <?php
-<<<<<<< HEAD
+/**
+ * Created by PhpStorm.
+ * User: max
+ * Date: 27/10/2017
+ * Time: 14:55
+ */
 
-  require_once("../lib/pdo.php");
+require_once("config.php");
+class Requete
+{
+    public static function connect() {
+        $dsn = "mysql:host=".HOST.";port=3306;dbname=".BASENAME;
+        $pdo = new PDO($dsn, LOGIN, PASSWORD,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+        $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+        $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
+        return $pdo;
+    }
 
-  function recherche_biere($biere){
-
-    $sql="SELECT type_de_biere.nom FROM type_de_biere VALUES(:biere)";
-      $stmt = $pdo->prepare($sql);
-      $stmt->bindParam(":biere", $biere, PDO::PARAM_STR);
-=======
-  require_once("config/config.php");
-  require_once(BASE_FILE."lib/pdo.php");
-
-  function recherche_biere($biere){
-
-    $sql="SELECT type_de_biere.nom FROM type_de_biere VALUES(:name)";
-      $stmt = $pdo->prepare($sql);
-      $stmt->bindParam(":name", $biere, PDO::PARAM_STR);
->>>>>>> origin/Quentin
-      $stmt->execute();
-  }
- ?>
+    public static function recherche_biere($biere){
+        $pdo = self::connect();
+        $sql="SELECT type_de_biere.nom FROM type_de_biere VALUES(:name)";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(":name", $biere, PDO::PARAM_STR);
+        $stmt->execute();
+    }
+}
