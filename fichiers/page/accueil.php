4c5626b9eee52e3da79eeabe3fed725c174373d7
Bluze <gosselinquentin@orange.fr>
2017-10-27 10:27:21 +0100

    ajout de fichier

---
+++ fichiers/page/accueil.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>LA BIBINE C'EST ICI</title>
+  </head>
+  <body>
+    <h1>FEMMME!!! A boire!</h1>
+    <p>je fais un petit test pour verifier et voir ce que je peux faire</p>
+  </body>
+</html>
