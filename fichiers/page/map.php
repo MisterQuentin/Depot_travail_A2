2a5bc96b3abcea8575b53b564fb1bb05f916da68
Totostore <totostore@gmail.com>
2017-10-27 12:04:46 +0200

    Map V1

--- fichiers/page/map.php
+++ fichiers/page/map.php
@@ -4,8 +4,10 @@
     <meta charset="utf-8">
     <title>Carte des Bars</title>
   </head>
+
   <body>
-    <h1>Mort De Reich !</h1>
-    <p>prout caca</p>
-  </body>
+    <h1>Je préfèrerais etre la-bas plutot qu'ici...</h1>
+	<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2607.9736431813303!2d-0.3676239179320031!3d49.18208674677159!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x480a42955f5b46eb%3A0x33dddc558bb641e5!2sLe+VERTIGO!5e0!3m2!1sfr!2sfr!4v1509098441749" width="800" height="600" frameborder="0" style="border:0" allowfullscreen></iframe>
+
+</body>
 </html>
